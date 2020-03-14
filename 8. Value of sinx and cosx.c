f1c4f1f5db1294e688bbe268891059212f83c035
shab-eeb <62139910+shab-eeb@users.noreply.github.com>
2020-03-14 14:15:17 +0530

    Create 8. Value of sinx and cosx.c

---
+++ 8. Value of sinx and cosx.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+void main ()
+  {
+    float sine, cosine, x; 
+    printf("\n Enter the value :");
+    scanf("%f",&x);
+    sine=sin(x);
+    cosine=cos(x);
+    printf("\nsin value is %f ",sine); 
+    printf("\ncos value is %f ",cosine); 
+ }
